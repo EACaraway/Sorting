51335ed21892dacc3a001119a5fcf532fe8144c1
Evan Caraway <evan.caraway@outlook.com>
2020-11-24 23:47:18 -0800

    Project Files

---
+++ sorting/ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] array1 = { 1,3,5,4,7};
+					   
+			for (int pointer1 = 0; pointer1 < array1.Length - 1;)
+			{
+
+				int pointer2 = pointer1 + 1;
+				
+				if (array1[pointer1] > array1[pointer2])
+				{
+					int temp = array1[pointer1];
+					array1[pointer1] = array1[pointer2];
+					array1[pointer2] = temp;
+					pointer1 = 0;
+				}
+				
+				else
+				{
+					pointer1++;
+				}
+								
+			}
+			foreach (int val in array1)
+			{
+				Console.WriteLine(val);
+			}
+
+		}
+
+		
+	}
+}
